4c6e2f34088bd500e68198099c95efad6e79cdd3
daphnechrisitna <daphne.prem@gmail.com>
2025-06-12 11:35:14 +0530

    origins

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import dalleRoutes from './routes/dalle.routes.js';
 
 const app = express();
 app.use(cors({ 
-                origin: 'http://localhost:5173', // Your Vite frontend
+                origin: 'https://shirt-design-omega.vercel.app/', // Your Vite frontend
                 exposedHeaders: ['Content-Type']}));
 app.use(express.json({ limit: '50mb' }));
 
